27382d1cb59b756301dc6e59fa55248a6b546c42
Manu Rana <manurana@hotmail.com>
2023-08-04 20:38:49 +0930

    interface declared in component itself

--- src/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { RowData } from "@tanstack/react-table";
-
-declare module "@tanstack/table-core" {
-  interface TableMeta<TData extends RowData> {
-    onEditTeamMember: () => void;
-  }
-}
